9dc3abd84c6cf6f26ac2682dfa2245468ceb428e
Thomas Hudson <g.t.hudson@durham.ac.uk>
2016-01-19 01:11:33 +0000

    Begun linking front end to API
    #Jira CEP-25 CEP-26 CEP-27 CEP-28 #time 3h

---
+++ static/app.js
@@ -0,0 +1,94 @@
+// app.js
+angular.module('sortApp', [])
+
+// Service
+.factory('ajax', ['$http', function($http) {
+  return {
+    getVolunteers: function() {
+      return $http.get('/api/volunteer/').success(function() {
+      });
+    },
+    addVolunteer: function() {
+      return $http.post('/api/volunteer/').success(function() {
+      });
+    },
+    updateVolunteers: function($params) {
+      $params._method = 'patch';
+      console.log("update method",$params )
+      if ($params.information && $params.information.name !== ""){
+        return $http.post('/api/volunteer/' + id, $params).success(function() {
+          delete $params._method;
+        });
+      }
+    },
+    deleteVolunteers: function(id) {
+        return $http.delete('/api/volunteer/' + id).success(function() {
+          console.log("deleted")
+        });
+    }
+
+};}])
+
+
+
+.controller('mainController', ['$scope', 'ajax', function($scope, serverComm) {
+  
+  $scope.sortType     = 'fName'; // set the default sort type
+  $scope.sortReverse  = false;  // set the default sort order
+  $scope.searchTerm   = '';     // set the default search/filter term
+
+  // create the list of volunteers 
+  $scope.volunteers = ""
+
+
+
+  
+$scope.getData = function(callback) {
+  serverComm.getVolunteers().success(function(data) {
+    if (typeof(data) === 'object') {
+      $scope.volunteers = data;
+      console.log("Get data", $scope.volunteers)
+    }
+    if (typeof callback !== "undefined"){
+      callback();
+    }
+  });
+}
+
+$scope.delete = function(id,index) {
+  serverComm.deleteVolunteers(id).success(function(data) {
+    //Remove record from $scope.volunteers
+    $scope.volunteers.splice(getIndexFromId(id), 1);
+  }); 
+}
+
+$scope.newVolunteer = function() {
+  serverComm.addVolunteer().success(function(data) {
+    //Add record to $scope.volunteers
+    $scope.volunteers.push(data)
+  });
+}
+
+$scope.change = function(id) {  
+  //
+}
+
+
+function getIndexFromId(id){
+  for (i = 0; i < $scope.volunteers.length; i++) { 
+      if($scope.volunteers[i]._id == id){
+        return i
+      }
+}
+}
+
+$scope.submitChange = function(id,index) {
+  console.log("change");
+
+  serverComm.updateVolunteers(id).success(function(data) {
+
+  });
+}
+
+    $scope.getData();
+}]);
