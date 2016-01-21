4529e8cef6807482ed73bf3efc8d1a37e338de67
Piggelinus <lericsson22@gmail.com>
2016-01-21 19:17:00 +0000

    Pagination working. Only error is with clicking the numbered links

--- static/app.js
+++ static/app.js
@@ -38,60 +38,86 @@ angular.module('sortApp', [])
   // create the list of volunteers 
   $scope.volunteers = ""
 
-
-
-  
-$scope.getData = function(callback) {
-  serverComm.getVolunteers().success(function(data) {
-    if (typeof(data) === 'object') {
-      $scope.volunteers = data;
-      console.log("Get data", $scope.volunteers)
-    }
-    if (typeof callback !== "undefined"){
-      callback();
-    }
-  });
-}
-
-$scope.delete = function(id,index) {
-  serverComm.deleteVolunteers(id).success(function(data) {
-    //Remove record from $scope.volunteers
-    $scope.volunteers.splice(getIndexFromId(id), 1);
-  }); 
-}
-
-$scope.newVolunteer = function() {
-  serverComm.addVolunteer().success(function(data) {
-    //Add record to $scope.volunteers
-    $scope.volunteers.push(data)
-  });
-}
-
-$scope.change = function(id) {  
-  //
-   volunteer = $scope.volunteers[getIndexFromId(id)];
-   serverComm.updateVolunteers(volunteer).success(function(data) {
-    //Add record to $scope.volunteers
-    $scope.volunteers.push(data)
-  });
-}
-
-
-function getIndexFromId(id){
-  for (i = 0; i < $scope.volunteers.length; i++) { 
-      if($scope.volunteers[i]._id == id){
-        return i
+  // Pagination variables
+  $scope.currentPage = 0;
+  $scope.pageSize = 10;
+  $scope.numberOfPages = function() {
+      return Math.ceil($scope.volunteers.length/$scope.pageSize);
+  }
+  $scope.rowsShown = function() {
+      if (($scope.currentPage * $scope.pageSize) + $scope.pageSize < $scope.volunteers.length) {
+          return Number(($scope.currentPage * $scope.pageSize) + $scope.pageSize);
+      } else {
+          return $scope.volunteers.length;
       }
   }
-}
+  $scope.getNumber = function(num) {
+      return Array.apply(null, {length: num}).map(Number.call, Number)
+  }
+  $scope.isActive = function(id) {
+      return id == $scope.currentPage ? 'active' : '';
+  }
+    
+    $scope.getData = function(callback) {
+    serverComm.getVolunteers().success(function(data) {
+        if (typeof(data) === 'object') {
+        $scope.volunteers = data;
+        console.log("Get data", $scope.volunteers)
+        }
+        if (typeof callback !== "undefined"){
+        callback();
+        }
+    });
+    }
 
-$scope.submitChange = function(id,index) {
-  console.log("change");
+    $scope.delete = function(id,index) {
+    serverComm.deleteVolunteers(id).success(function(data) {
+        //Remove record from $scope.volunteers
+        $scope.volunteers.splice(getIndexFromId(id), 1);
+    }); 
+    }
 
-  serverComm.updateVolunteers(id).success(function(data) {
+    $scope.newVolunteer = function() {
+    serverComm.addVolunteer().success(function(data) {
+        //Add record to $scope.volunteers
+        $scope.volunteers.push(data)
+    });
+    }
 
-  });
-}
+    $scope.change = function(id) {  
+    //
+    volunteer = $scope.volunteers[getIndexFromId(id)];
+    serverComm.updateVolunteers(volunteer).success(function(data) {
+        //Add record to $scope.volunteers
+        $scope.volunteers.push(data)
+    });
+    }
+
+
+    function getIndexFromId(id){
+    for (i = 0; i < $scope.volunteers.length; i++) { 
+        if($scope.volunteers[i]._id == id){
+            return i
+        }
+    }
+    }
+
+    $scope.submitChange = function(id,index) {
+    console.log("change");
+
+    serverComm.updateVolunteers(id).success(function(data) {
+
+    });
+    }
 
     $scope.getData();
-}]);
+}])
+
+//We already have a limitTo filter built-in to angular,
+//let's make a startFrom filter
+.filter('startFrom', function() {
+    return function(input, start) {
+        start =+ start; //parse to int
+        return input.slice(start);
+    }
+});
